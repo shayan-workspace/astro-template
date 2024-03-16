ff41cb79637eec9c5b6d63bd92f8bd9a3a6cc6ad
shayan <shayan.workspace@gmail.com>
2024-03-16 10:06:25 +0500

    astro + react + svelte + vue + nanostores

--- src/components/svelte/theme/theme-toggle.svelte
+++ src/components/svelte/theme/theme-toggle.svelte
@@ -12,10 +12,10 @@
 
 <Button on:click={toggleTheme} variant="outline" size="icon">
   <Sun
-    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    class="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
   />
   <Moon
-    class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    class="absolute h-[1.2rem] w-[1.2rem] dark:-rotate-90 scale-100 transition-all rotate-0 dark:scale-0"
   />
   <span class="sr-only">Toggle theme</span>
 </Button>
--- src/components/vue/theme/theme-toggle.vue
+++ src/components/vue/theme/theme-toggle.vue
@@ -17,8 +17,8 @@
 
 <template>
   <Button @click="toggleTheme" variant="outline" size="icon">
-    <Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-    <Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <Sun class="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <Moon class="absolute h-[1.2rem] w-[1.2rem] dark:-rotate-90 scale-100 transition-all rotate-0 dark:scale-0" />
     <span class="sr-only">Toggle theme</span>
   </Button>
 </template>
